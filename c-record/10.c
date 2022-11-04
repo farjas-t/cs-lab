bdb236c93fe3416e6e6301d6cc497ec3e03c83cf
Farjas T <farjasktr@gmail.com>
2022-11-04 11:02:49 +0530

    Update 10.c

--- c-record/10.c
+++ c-record/10.c
@@ -2,15 +2,18 @@
 #include<stdio.h>
 int main()
 {
-	int a,d,n=0,i=1;
-	static int b[9]={1000,500,100,50,20,10,5,2,1};
+	int a,i=0,d,n=0;
+	static int b[ ]={2000,500,100,50,20,10,5,2,1};
 	printf("Enter the amount:");
 	scanf("%d",&a);
 	while(a>0)
 	{
-		if(a>=b[i])
-		{
-			
-		}
+		d=a/b[i];
+		printf("Number of %d notes is %d\n",b[i],d);
+		n=n+d;
+		a=a%b[i];
+		i++;
 	}
+	printf("Total number of currency notes=%d\n",n);
+
 }
