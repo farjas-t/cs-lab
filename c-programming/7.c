d40d80e9bfa543b601111b96e57c4c662f30248b
Farjas T <farjasktr@gmail.com>
2022-11-16 22:22:26 +0530

    updated c programs

--- c-programming/7.c
+++ c-programming/7.c
@@ -1,16 +1,14 @@
 //7-Find the sum of the series S = 1 + (½)2 + (1/3)3 +.... to 0.0001% accuracy
-#include <math.h>
 #include <stdio.h>
-int main()
+#include <math.h>
+int main
 {
-	int n,i;
-	double sums = 0.0, ser;
-	printf("Enter number of terms: ");
-	scanf("%d",&n);
-	for (i = 1; i <= n; ++i) {
-		ser = 1 / pow(i, i);
-		sums+=ser;
+	double term,sum=0;
+	int i=1,flag;
+	for(i=1;i<=10;++i){
+		term=1/pow(i,i);
+		sum+=term;
 	}
-	printf("Sum of %d terms : %.5f",n,sums);
-	return 0;
+	if(sum>0.000001)
+		printf("Sum of series=%f",sum);
 }
