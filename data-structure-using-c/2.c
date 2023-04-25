294cfd3c7e668ae677bcbc63fda5f7d55a2835e3
Farjas T <farjasktr@gmail.com>
2023-04-25 11:21:19 +0530

    Update 2.c

--- data-structure-using-c/2.c
+++ data-structure-using-c/2.c
@@ -1,26 +1,21 @@
-//3- Implement Pattern matching algorithm
-#include <stdio.h> 
-#include <string.h> 
-int main() 
-{ 
-    char text[20],pat[20];
-    int a,b,i,j,flag=0;
-    printf("Enter the string : ");
-    gets(text);
-    printf("Enter the pattern to find : ");
-    gets(pat);
-    a = strlen(pat); 
-    b = strlen(text);
-    for (i = 0; i <= b - a; i++) {
-        for (j = 0; j < a; j++)
-            if (text[i + j] != pat[j])
-                break;
-        if (j == a){
-            printf("Pattern found at position %d \n", i+1);
-            flag=1;
-		} 
-    } 
-    if(flag==0)
-		printf("Pattern not found!");
-    return 0;
+//2- Reverse a String using Pointers
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char str[20], ch, *begin_ptr, *end_ptr;
+	int len, i;
+	printf("Enter a string : ");
+	gets(str);
+	len = strlen(str);
+	begin_ptr = str;
+	end_ptr = str + len - 1;
+	for (i = 0; i < (len - 1) / 2; i++) {
+		ch = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = ch;
+		begin_ptr++;
+		end_ptr--;
+	}
+	printf("Reverse of the string: %s\n", str);
 }
