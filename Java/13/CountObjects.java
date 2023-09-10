688f982c3ae2f1be0481ae6bafe1c570f84c6543
Farjas T <farjasktr@gmail.com>
2023-09-11 00:21:09 +0530

    corrected 13

--- Java/13/CountObjects.java
+++ Java/13/CountObjects.java
@@ -5,7 +5,7 @@ public class CountObjects {
     }
     public static void main(String[] args) {
         CountObjects obj1 = new CountObjects();
-        CountObjects obj2 = new CountObjects(9);
+        CountObjects obj2 = new CountObjects();
         CountObjects obj3 = new CountObjects();
         System.out.println("Number of objects created : " + CountObjects.count);
     }
